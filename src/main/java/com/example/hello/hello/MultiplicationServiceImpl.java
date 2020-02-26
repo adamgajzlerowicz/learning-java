9278d2a4b27c80dae0424d1ee913aeb7ca88b2d5
Adam Gajzlerowicz <nelf86@gmail.com>
2020-02-26 11:22:42 +0000

    test passing

---
+++ src/main/java/com/example/hello/hello/MultiplicationServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.hello.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MultiplicationServiceImpl implements MultiplicationService {
+    private RandomGeneratorService randomGeneratorService;
+
+    @Autowired
+    public MultiplicationServiceImpl(RandomGeneratorService randomGeneratorService) {
+        this.randomGeneratorService = randomGeneratorService;
+    }
+
+    @Override
+    public Multiplication createRandomMultiplication() {
+        int factorA = this.randomGeneratorService.generateRandomFactor();
+        int factorB = this.randomGeneratorService.generateRandomFactor();
+
+        return new Multiplication(factorA, factorB);
+    }
+}
